e68a3373ca8646d07a0e71326025c127e007151e
James Maldaner <maldaner@ualberta.ca>
2026-07-28 13:56:49 -0600

    Basic plotting, still need to make the actual functions

--- main.py
+++ main.py
@@ -1,12 +1,19 @@
 from dataclasses import dataclass
 
 import numpy as np
+import matplotlib.pyplot as plt
 
 @dataclass
 class pulse_stretch_class:
-    center_lambda: float = 256e-9
-    fwhm_lambda: float = 0.8e-9
-    gdd_init: float = 0
+    center_lambda: float = 256e-9  # meter
+    fwhm_lambda: float = 0.8e-9  # meter
+    gdd_init: float = 0  # s^2
+
+    n_points: int = 2 ** 5  # []
+    t_max: float = 1e-9  # second
+
+    def __post_init__(self):
+        self.init_arrays()
 
     def __str__(self):
         str_str = f"Center Wavelength: {self.center_lambda*1e9} nm\n"
@@ -14,6 +21,12 @@ class pulse_stretch_class:
         str_str += f"Initial GDD: {self.gdd_init}\n"
         return str_str
 
+    def init_arrays(self):
+        t = np.linspace(-self.t_max, self.t_max, self.n_points)
+        F = t
+        plt.plot(t, F)
+        plt.show()
+        
 
 
 
