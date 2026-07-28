9c428aa329a7dfc8904e06f402b3bdd68d489c89
James Maldaner <maldaner@ualberta.ca>
2026-07-28 13:40:49 -0600

    updated main

--- main.py
+++ main.py
@@ -3,11 +3,15 @@ from dataclasses import dataclass
 import numpy as np
 
 @dataclass
-def class:
+class pulse_stretch_class:
     center_lambda: float = 256e-9
+    fwhm_lambda: float = 0.8e-9
+    gdd_init: float = 0
 
     def __str__(self):
-        str_str = f"Center Wavelength: {self.center_lambda*1e9} nm"
+        str_str = f"Center Wavelength: {self.center_lambda*1e9} nm\n"
+        str_str += f"FWHM Wavelength: {self.fwhm_lambda*1e9} nm\n"
+        str_str += f"Initial GDD: {self.gdd_init}\n"
         return str_str
 
 
@@ -15,7 +19,7 @@ def class:
 
 def main():
     """Main method."""
-    test_class = class()
+    test_class = pulse_stretch_class()
 
     print(test_class)
 
