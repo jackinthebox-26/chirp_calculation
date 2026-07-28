dcc9b5e875cf0dbcd6fef558a251f20b022347ee
James Maldaner <maldaner@ualberta.ca>
2026-07-28 13:17:07 -0600

    Started the clas and the main section

--- main.py
+++ main.py
@@ -1,2 +1,25 @@
+from dataclasses import dataclass
+
 import numpy as np
+
+@dataclass
+def class:
+    center_lambda: float = 256e-9
+
+    def __str__(self):
+        str_str = f"Center Wavelength: {self.center_lambda*1e9} nm"
+        return str_str
+
+
+
+
+def main():
+    """Main method."""
+    test_class = class()
+
+    print(test_class)
+
+
+if __name__ == "__main__":
+    main()
 
